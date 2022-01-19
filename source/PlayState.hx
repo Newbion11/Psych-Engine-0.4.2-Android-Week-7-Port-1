64ad21a5b9ea420cd0c59c5b2bbc824c34ac1599
Newbion1919 <97095173+Newbion1919@users.noreply.github.com>
2022-01-19 23:04:11 +0900

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -901,6 +901,14 @@ class PlayState extends MusicBeatState
 		add(iconP2);
 		reloadHealthBarColors();
 
+        var creditTxt:FlxText = new FlxText(4,healthBarBG.y + 20,0,("Port by Newbion"), 24);
+        creditTxt.scrollFactor.set();
+        creditTxt.setFormat("VCR OSD Mono", 24, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+        creditTxt.borderColor = FlxColor.BLACK;
+        creditTxt.borderSize = 3;
+        creditTxt.borderStyle = FlxTextBorderStyle.OUTLINE;
+        add(creditTxt);
+
 		scoreTxt = new FlxText(0, healthBarBG.y + 36, FlxG.width, "", 20);
 		scoreTxt.setFormat(Paths.font("vcr.ttf"), 20, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		scoreTxt.scrollFactor.set();
